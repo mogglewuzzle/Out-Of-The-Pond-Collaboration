9d8fd793eabace9a0c693a4432700a5f8d385dda
Matthew Monty <mjlmonty@utas.edu.au>
2026-05-29 20:33:15 +1000

    finished new tutorial

--- Assets/Scripts/Object_Consumable.cs
+++ Assets/Scripts/Object_Consumable.cs
@@ -10,8 +10,18 @@ public class Object_Consumable : MonoBehaviour
     [Tooltip("Optional. If empty, this object is deactivated when consumed.")]
     [SerializeField] private GameObject objectToDeactivate;
 
+    [Header("Scene Load")]
+    [SerializeField] private bool loadSceneOnConsume;
+    [Tooltip("Scene name to load when consumed. The scene must be added to Build Settings.")]
+    [SerializeField] private string sceneName;
+    [Tooltip("If enabled, Scene Build Index is used instead of Scene Name.")]
+    [SerializeField] private bool useSceneBuildIndex;
+    [SerializeField] private int sceneBuildIndex;
+
     [Header("Events")]
     [SerializeField] private UnityEvent onConsumed;
+
+    private bool sceneLoadRequested;
 
     public void Consume(GameObject consumer)
     {
@@ -25,6 +35,7 @@ public class Object_Consumable : MonoBehaviour
             Systems_ConsumedItemTracker.Instance.RecordConsumed(consumedObject);
 
         onConsumed?.Invoke();
+        TryLoadSceneOnConsume();
 
         if (!deactivateOnConsume)
             return;
@@ -34,5 +45,29 @@ public class Object_Consumable : MonoBehaviour
             : consumedObject;
 
         targetObject.SetActive(false);
+    }
+
+    private void TryLoadSceneOnConsume()
+    {
+        if (!loadSceneOnConsume || sceneLoadRequested)
+            return;
+
+        Systems_SceneManager sceneManager = Systems_SceneManager.Instance;
+        if (sceneManager == null)
+        {
+            sceneManager = FindFirstObjectByType<Systems_SceneManager>();
+            if (sceneManager == null)
+            {
+                Debug.LogWarning($"{nameof(Object_Consumable)} on {name} cannot load scene after consume: no {nameof(Systems_SceneManager)} found.", this);
+                return;
+            }
+        }
+
+        sceneLoadRequested = true;
+
+        if (useSceneBuildIndex)
+            sceneManager.LoadScene(sceneBuildIndex);
+        else
+            sceneManager.LoadScene(sceneName);
     }
 }
